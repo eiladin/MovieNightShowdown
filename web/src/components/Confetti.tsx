571837710b002cde4039b03e03616a8d39f12371
Sami Khan <eiladin@gmail.com>
2026-07-22 21:52:18 -0500

    fix(web): use setInterval for confetti to prevent browser lockup

--- web/src/components/Confetti.tsx
+++ web/src/components/Confetti.tsx
@@ -6,27 +6,29 @@ export default function Confetti() {
     const duration = 3000
     const end = Date.now() + duration
 
-    const frame = () => {
+    const interval = setInterval(() => {
+      if (Date.now() > end) {
+        clearInterval(interval)
+        return
+      }
+
       confetti({
-        particleCount: 5,
+        particleCount: 20,
         angle: 60,
         spread: 55,
         origin: { x: 0 },
         colors: ['#26ccff', '#a25afd', '#ff5e7e', '#88ff5a', '#fcff42', '#ffa62d', '#ff36ff']
       })
       confetti({
-        particleCount: 5,
+        particleCount: 20,
         angle: 120,
         spread: 55,
         origin: { x: 1 },
         colors: ['#26ccff', '#a25afd', '#ff5e7e', '#88ff5a', '#fcff42', '#ffa62d', '#ff36ff']
       })
+    }, 250)
 
-      if (Date.now() < end) {
-        requestAnimationFrame(frame)
-      }
-    }
-    frame()
+    return () => clearInterval(interval)
   }, [])
 
   return null
